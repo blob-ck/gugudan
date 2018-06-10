965a0c500f5ecc9b03bc06875b6472af2c6f0910
smallonestep <hankwan777@gmail.com>
2018-06-10 14:25:21 +0900

    반복출력문을 메소드 작성하여 중복제거, 키보드 입력값을 1과 2만 입력받도록 제한하기 위해 재귀 사용

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,6 +1,31 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class Gugudan {
+	
+	void menu() {
+		System.out.println("");
+		System.out.println("숫자를 입력하세요");
+		System.out.println("1 - 다시 입력");
+		System.out.println("2 - 종료 ");
+	}
+	
+	//재귀로 1과 2 만 입력받도록 했다.
+	int con(int i) {
+		
+		if(i != 1 && i !=2) {
+			System.out.println("");
+			System.out.println("1과 2 중 선택하세요");
+			
+			//반복되는 출력문
+			menu();
+			
+			return con(new Scanner(System.in).nextInt());
+		}
+		
+		return i;
+	}
 
 	public static void main(String[] args) {
 		
@@ -22,7 +47,6 @@ public class Gugudan {
 			System.out.println(2 * i);
 		}
 		
-		
 		//3단
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(3 * i);
@@ -31,12 +55,42 @@ public class Gugudan {
 		
 		
 		//반복문 2개 사용하여 원하는 단수와 곱하는 수 범위 자동 출력 - 범위만 정하면 된다
-		for (int i = 2; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
-				System.out.print(j + " x " + i + " = " + i*j + "	");
+//		for (int i = 2; i <= 9; i++) {
+//			for (int j = 1; j <= 9; j++) {
+//				System.out.print(j + " x " + i + " = " + i*j + "	");
+//			}
+//			System.out.println("");
+//		}
+		
+		
+		
+		//이제 키보드로 입력받아 출력해보자
+		Gugudan ggd = new Gugudan();
+		//int button = 1;
+		//while(button == 1){
+		//변수를 사용하지 않도록 고쳐봤다 - button 없앰
+		while(true){
+			
+			System.out.println("구구단 중 출력할 단 입력 : ");
+			
+			Scanner sc = new Scanner(System.in);
+			int dan = sc.nextInt();
+			System.out.println("몇 배까지 출력? : ");
+			int times = sc.nextInt();
+			
+			for (int i = 1; i <= times; i++) {
+				System.out.println(dan + " x " + i + " = " + i*dan);
 			}
-			System.out.println("");
+			
+			//반복되는 출력문을 따로 메소드로 만들었다.
+			ggd.menu();
+
+			//button = ggd.con(sc.nextInt());
+			
+			//if(button == 2){
+			if(ggd.con(sc.nextInt()) == 2){
+				break;
+			}
 		}
 	}
-
 }
